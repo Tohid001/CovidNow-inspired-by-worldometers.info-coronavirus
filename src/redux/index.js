37b989564d3d9decdcb3ed35fe2e5e603742ac29
Tohidul Islam <mdtohid.sust@gmail.com>
2021-08-06 02:27:38 +0600

    earranged the whole thing using context api

---
+++ src/redux/index.js
@@ -0,0 +1,14 @@
+// // import axios from "axios";
+
+// const myFunc = async () => {
+//   try {
+//     const data = await new Promise((res, rej) => {
+//       const a = null;
+//       a ? res("hello") : rej("oh no!!");
+//     });
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// myFunc();
